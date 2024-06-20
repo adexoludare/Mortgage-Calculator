ccbfe5255506e0bfd26c395d1f8340d1cc330467
Adedipupo Oludare <adexoludare@gmail.com>
2024-06-20 11:42:51 +0100

    Refactor repetitive patterns

--- src/App.java
+++ src/App.java
@@ -6,52 +6,49 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Mortgage Calculator 1.0");
 
-        final byte months_in_year = 12;
-        final byte percent = 100;
+        int principal = (int) readNumber("Principal ($1k - $1M): ", 1_000, 1_000_000);
+        float annualIntrestRate = (float) readNumber("Annual Interest Rate(0% - 30%) : ", 0.1, 30);
+        byte years = (byte) readNumber("Period (Years): ", 1, 30);
 
-        int principal;
-        double annualIntrestRate;
-        byte years;
-
-        Scanner Scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Principal ($1k - $1M): ");
-            principal = Scanner.nextInt();
-            if (principal >= 1_000 && principal <= 1_000_000)
-                break;
-            System.out.println("Enter a value between 1000 and 1000000");
-        }
-
-        while (true) {
-            System.out.print("Annual Intrest Rate(0% -30%) : ");
-            annualIntrestRate = Scanner.nextDouble();
-            if (annualIntrestRate > 0 && annualIntrestRate <= 30)
-                break;
-            System.out.println("Invalid Rate! Enter a value greater than 0 and less than or equal to 30");
-        }
-        while (true) {
-            System.out.print("Period (Years): ");
-            years = Scanner.nextByte();
-            if (years > 0 && years <= 30) {
-                break;
-            }
-            System.out.println("Enter a value greater than 0 and less than or equal to 30");
-        }
-
-        int numberOfPayment = years * months_in_year;
         System.out.println(
                 "You are requeting for a loan : $" + principal + " at a rate :" + annualIntrestRate + " for : "
                         + years);
 
-        double monthlyItrest = (annualIntrestRate / percent) / months_in_year;
-
-        double mortgage = principal *
-                (monthlyItrest * Math.pow(1 + monthlyItrest, numberOfPayment)
-                        / (Math.pow(1 + monthlyItrest, numberOfPayment) - 1));
+        double mortgage = calculateMortgage(principal, annualIntrestRate, years);
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println(mortgageFormatted);
 
     }
 
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value = 0;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextDouble();
+            if (value >= min && value <= max)
+                break;
+            System.out
+                    .println("Invalid Input! Enter a value between " + min + " and  " + max);
+        }
+        return value;
+    }
+
+    public static double calculateMortgage(
+            int principal,
+            float annualIntrestRate,
+            byte years) {
+        final byte months_in_year = 12;
+        final byte percent = 100;
+
+        short numberOfPayment = (short) (years * months_in_year);
+        double monthlyItrest = (annualIntrestRate / percent) / months_in_year;
+
+        double mortgage = principal *
+                (monthlyItrest * Math.pow(1 + monthlyItrest, numberOfPayment)
+                        / (Math.pow(1 + monthlyItrest, numberOfPayment) - 1));
+
+        return mortgage;
+    }
 }
